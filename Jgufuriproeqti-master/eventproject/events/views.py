e02ad5b27631f0bf7f62f69845c4ea1daf347fd5
levani <cryoffearenjoyer@gmail.com>
2026-02-10 15:26:19 +0400

    .

---
+++ Jgufuriproeqti-master/eventproject/events/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Event
+from .forms import EventForm
+
+def event_list(request):
+    events = Event.objects.all()
+    return render(request, 'events/event_list.html', {'events': events})
+
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    return render(request, 'events/event_detail.html', {'event': event})
+
+def event_create(request):
+    form = EventForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('event_list')
+    return render(request, 'events/event_form.html', {'form': form})
+
+def event_edit(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    form = EventForm(request.POST or None, instance=event)
+    if form.is_valid():
+        form.save()
+        return redirect('event_list')
+    return render(request, 'events/event_form.html', {'form': form})
+
+def event_delete(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    event.delete()
+    return redirect('event_list')
